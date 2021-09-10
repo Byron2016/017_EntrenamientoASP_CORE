4c17a730e4422c672939566da1c6034cc83925ec
Byron 2016 <bgva2005@yahoo.com>
2021-09-10 16:13:49 -0500

    V_017_HeredaController y RetornarJson

--- SolEmptyWebApplicationCore/ProjEmptyWebApplicationCore/Controllers/HomeController.cs
+++ SolEmptyWebApplicationCore/ProjEmptyWebApplicationCore/Controllers/HomeController.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ProjEmptyWebApplicationCore.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public string Index()
+        public JsonResult Index()
         {
-            return "Hello from MVC";
+            //return "Hello from MVC";
+            return Json(new { id= 1, name = "Trump" });
         }
     }
 }
