864e8eb65945c05941b6d4e17c0e5bb4b0f146ce
Byron 2016 <bgva2005@yahoo.com>
2021-09-12 20:28:02 -0500

    V_032_Routing_Conventional routing

---
+++ SolEmptyWebApplicationCore/ProjEmptyWebApplicationCore/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjEmptyWebApplicationCore.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public string Index()
+        {
+            return "List() de DepartamentsController";
+        }
+
+        public string Details()
+        {
+            return "Details() de DepartamentsController";
+        }
+    }
+}
