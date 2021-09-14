05479664de183d8f87b459980e11ed1cfe831fbf
Byron 2016 <bgva2005@yahoo.com>
2021-09-14 18:41:12 -0500

    V_57_Error 404 id no existe

--- SolEmptyWebApplicationCore/ProjEmptyWebApplicationCore/Controllers/HomeController.cs
+++ SolEmptyWebApplicationCore/ProjEmptyWebApplicationCore/Controllers/HomeController.cs
@@ -29,16 +29,24 @@ namespace ProjEmptyWebApplicationCore.Controllers
             return View(model);
         }
 
-        public ViewResult Details(int? Id)
+        public ViewResult Details(int? id)
         {
+            Employee employee = _employeeRepository.GetEmployee(id.Value);
+
+            if(employee == null)
+            {
+                Response.StatusCode = 404;
+                return View("EmployeeNotFound", id.Value);
+            }
+
             HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
             {
-                Employee = _employeeRepository.GetEmployee(Id??1),
+                Employee = _employeeRepository.GetEmployee(id??1),
                 PageTitle = "Employee Details"
             };
             return View(homeDetailsViewModel);
         }
-
+        
         [HttpGet]
         public ViewResult Create()
         {
