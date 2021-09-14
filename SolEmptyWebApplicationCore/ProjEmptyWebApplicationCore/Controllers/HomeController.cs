9e89f2c45324c9c5260d598ae9b7d564613e17e6
Byron 2016 <bgva2005@yahoo.com>
2021-09-14 18:24:18 -0500

    V_56_Edit Post

--- SolEmptyWebApplicationCore/ProjEmptyWebApplicationCore/Controllers/HomeController.cs
+++ SolEmptyWebApplicationCore/ProjEmptyWebApplicationCore/Controllers/HomeController.cs
@@ -59,23 +59,73 @@ namespace ProjEmptyWebApplicationCore.Controllers
             return View(employeeEditViewModel);
         }
 
+        [HttpPost]
+        public IActionResult Edit(EmployeeEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Employee employee = _employeeRepository.GetEmployee(model.Id);
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Department = model.Department;
+                if(model.Photos != null)
+                {
+                    if(model.ExistingPhotoPath != null)
+                    {
+                        string filePath = Path.Combine(webHostEnvironment.WebRootPath, "images", model.ExistingPhotoPath);
+                        System.IO.File.Delete(filePath);
+                    }
+                    employee.PhotoPath = ProcessUploadedFile(model);
+                    
+                }
+
+                _employeeRepository.Update(employee);
+                return RedirectToAction("index");
+            }
+
+            return View();
+        }
+
+        private string ProcessUploadedFile(EmployeeCreateViewModel model)
+        {
+            string uniqueFileName = null;
+            if (model.Photos != null && model.Photos.Count > 0)
+            {
+                foreach (IFormFile photo in model.Photos)
+                {
+                    string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    using (var fileStream = new FileStream(filePath, FileMode.Create)) {
+                        // Esto es para que el archivo se libere
+                        // y podamos actualizar apenas lo creemos.
+
+                        
+                        photo.CopyTo(fileStream);
+                    }
+                    
+                }
+            }
+
+            return uniqueFileName;
+        }
 
         [HttpPost]
         public IActionResult Create(EmployeeCreateViewModel model)
         {
             if (ModelState.IsValid) 
             {
-                string uniqueFileName = null;
-                if(model.Photos != null && model.Photos.Count > 0)
-                {
-                    foreach (IFormFile photo in model.Photos)
-                    {
-                        string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
-                        uniqueFileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
-                        string filePath = Path.Combine( uploadsFolder, uniqueFileName);
-                        photo.CopyTo(new FileStream(filePath, FileMode.Create));
-                    }
-                }
+                string uniqueFileName = ProcessUploadedFile(model); ;
+                //if(model.Photos != null && model.Photos.Count > 0)
+                //{
+                //    foreach (IFormFile photo in model.Photos)
+                //    {
+                //        string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                //        uniqueFileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
+                //        string filePath = Path.Combine( uploadsFolder, uniqueFileName);
+                //        photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                //    }
+                //}
                 Employee newEmployee = new Employee
                 {
                     Name = model.Name,
