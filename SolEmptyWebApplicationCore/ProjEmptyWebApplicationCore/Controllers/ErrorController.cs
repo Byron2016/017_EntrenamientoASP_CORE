449268792c66dd0eb722dfcddb0b07acdd97f63a
Byron 2016 <bgva2005@yahoo.com>
2021-09-15 11:54:37 -0500

    V_58_app.UseStatusCodePagesWithReExecute con Path y QueryString

--- SolEmptyWebApplicationCore/ProjEmptyWebApplicationCore/Controllers/ErrorController.cs
+++ SolEmptyWebApplicationCore/ProjEmptyWebApplicationCore/Controllers/ErrorController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,14 @@ namespace ProjEmptyWebApplicationCore.Controllers
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHander(int statusCode)
         {
+            var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+
             switch (statusCode)
             {
                 case 404:
                     ViewBag.ErrorMessage = "Sorry, recurso solicitado no encontra";
+                    ViewBag.Path = statusCodeResult.OriginalPath;
+                    ViewBag.QS = statusCodeResult.OriginalQueryString;
                     break;
             }
             return View("NotFound");
